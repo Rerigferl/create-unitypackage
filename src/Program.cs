a2ba002a56ee6d5566f3d228054fa701dc5dedbe
Rerigferl <70315656+AshleyScarlet@users.noreply.github.com>
2024-06-15 14:18:04 +0900

    :ok: 2

--- src/Program.cs
+++ src/Program.cs
@@ -2,10 +2,15 @@
 using System.Buffers.Text;
 using System.Formats.Tar;
 using System.IO.Compression;
+using System.IO.Hashing;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.Unicode;
 
-ConsoleApp.Run(args, Commands.Root);
+unsafe
+{
+    ConsoleApp.Run(args, &Commands.Root);
+}
 
 static class Commands
 {
@@ -15,53 +20,40 @@ static class Commands
     /// <param name="input">Input directory path.</param>
     /// <param name="output">Output file path.</param>
     /// <param name="rootDirectory">-r, Root directory</param>
-    public static int Root([Argument] string input, [Argument] string output, string? rootDirectory = null)
+    /// <param name="excludeBaseDirectory">-e, Exclude base directory</param>
+    /// <param name="generateGuid">-g, Generate GUID automatically</param>
+    public static int Root([Argument] string input, [Argument] string output, string? rootDirectory = null, bool excludeBaseDirectory = false, bool generateGuid = false, string[]? ignores = null)
     {
+        input = Path.GetFullPath(input);
         if (!Directory.Exists(input))
         {
             Console.Error.WriteLine("Invalid input");
             return 1;
         }
 
-        var entries = Directory.EnumerateFiles(input, ".meta*", SearchOption.AllDirectories);
+        var assetEntries = AssetEntry.GetEntries(input, ignores);
 
-        using var destination = File.Create(output);
-        using var gz = new GZipStream(destination, CompressionLevel.SmallestSize);
-        using var tar = new TarWriter(gz, TarEntryFormat.Pax);
+        using var tar = new TarWriter(new GZipStream(File.Create(output), CompressionLevel.SmallestSize), TarEntryFormat.Ustar);
 
-        var guidBuffer = (stackalloc byte[128]);
         var pathNameBuffer = GC.AllocateUninitializedArray<byte>(8192);
 
-        foreach (var metaPath in entries)
+        foreach (ref var asset in assetEntries.AsSpan())
         {
-            Guid guid;
-            try
-            {
-                using (var fs = File.OpenRead(metaPath))
-                    fs.ReadExactly(guidBuffer);
-                var line2 = guidBuffer[(guidBuffer.IndexOf("\n"u8) + 1)..];
-                var guidSection = line2[(line2.IndexOf(":"u8) + 1)..line2.IndexOf("\n"u8)].Trim(" "u8);
-                if (!Utf8Parser.TryParse(guidSection, out guid, out _, 'N'))
-                    continue;
-            }
-            catch
-            {
-                continue;
-            }
+            if (string.IsNullOrEmpty(asset.MetaPath) && !generateGuid)
+                continue; 
 
-            var assetPath = metaPath[..^".meta".Length];
-            var filePath = Path.Join(rootDirectory, metaPath.AsSpan()[(Path.GetDirectoryName(input.AsSpan()).Length + 1)..])[..^".meta".Length].Replace("\\", "/");
+            var filePath = Path.Join(rootDirectory, asset.FilePath.AsSpan()[((excludeBaseDirectory ? input.AsSpan().TrimEnd(@"\/").Length : Path.GetDirectoryName(input.AsSpan()).Length) + 1)..]).Replace("\\", "/");
 
-            tar.WriteEntry(new PaxTarEntry(TarEntryType.Directory, guid.ToString("N")));
+            tar.WriteEntry(new UstarTarEntry(TarEntryType.Directory, $"{asset.Guid:N}/"));
 
-            var entry = new PaxTarEntry(TarEntryType.RegularFile, $"{guid:N}/asset.meta");
+            var entry = new UstarTarEntry(TarEntryType.RegularFile, $"{asset.Guid:N}/asset.meta");
             {
-                using var stream = File.OpenRead(metaPath);
+                using var stream = asset.GetMetaFileStream();
                 entry.DataStream = stream;
                 tar.WriteEntry(entry);
             }
 
-            entry = new PaxTarEntry(TarEntryType.RegularFile, $"{guid:N}/pathName");
+            entry = new(TarEntryType.RegularFile, $"{asset.Guid:N}/pathname");
             {
                 try
                 {
@@ -81,15 +73,83 @@ static class Commands
                 }
             }
 
-            if (File.Exists(assetPath))
+            if (File.Exists(asset.FilePath))
             {
-                entry = new(TarEntryType.RegularFile, $"{guid:N}/asset");
-                using var stream = File.OpenRead(assetPath);
+                entry = new(TarEntryType.RegularFile, $"{asset.Guid:N}/asset");
+                using var stream = File.OpenRead(asset.FilePath);
                 entry.DataStream = stream;
                 tar.WriteEntry(entry);
             }
         }
 
         return 0;
+    }
+}
+
+internal struct AssetEntry
+{
+    public string FilePath;
+    public string? MetaPath;
+    public Guid Guid;
+
+    public static AssetEntry[] GetEntries(string path, string[]? ignores = null)
+    {
+        var entries = Directory.EnumerateFileSystemEntries(path, "*", new EnumerationOptions() { AttributesToSkip = FileAttributes.Hidden, RecurseSubdirectories = true }).ToArray();
+        var ignoreItems = ignores is null ? new HashSet<string>() : ignores.Select(Path.GetFullPath).ToHashSet();
+        List<AssetEntry> list = [];
+        var guidBuffer = (stackalloc byte[128]);
+        for (int i = 0; i < entries.Length; i++)
+        {
+            var entry = entries[i];
+
+            if (entry.Contains(".git") || entry.EndsWith(".meta"))
+                continue;
+
+            string next = (i + 1 >= entries.Length) ? string.Empty : entries[i + 1];
+            var result = new AssetEntry()
+            {
+                FilePath = entry,
+                MetaPath = next,
+            };
+            if (!next.EndsWith(".meta"))
+            {
+                XxHash128.Hash(MemoryMarshal.AsBytes(entry.AsSpan()[(path.Length + (Path.EndsInDirectorySeparator(path) ? 0 : 1))..]), guidBuffer);
+                result.Guid = MemoryMarshal.Read<Guid>(guidBuffer);
+                result.MetaPath = null;
+            }
+            else
+            {
+                using (var fs = File.OpenRead(next))
+                    fs.ReadExactly(guidBuffer);
+                var line2 = guidBuffer[(guidBuffer.IndexOf("\n"u8) + 1)..];
+                var guidSection = line2[(line2.IndexOf(":"u8) + 1)..line2.IndexOf("\n"u8)].Trim(" "u8);
+                if (!Utf8Parser.TryParse(guidSection, out Guid guid, out _, 'N'))
+                    continue;
+                result.Guid = guid;
+            }
+
+            list.Add(result);
+        }
+        return [.. list];
+    }
+
+    public readonly Stream GetMetaFileStream()
+    {
+        if (File.Exists(MetaPath))
+            return File.OpenRead(MetaPath);
+
+        var stream = new MemoryStream(256);
+        stream.SetLength(stream.Capacity);
+        stream.TryGetBuffer(out var buffer);
+
+        Utf8.TryWrite(buffer, null, $$"""
+            fileFormatVersion: 2
+            guid: {{Guid:N}}
+            """, out var bytesWritten);
+
+        stream.SetLength(bytesWritten);
+        stream.Position = 0;
+
+        return stream;
     }
 }
